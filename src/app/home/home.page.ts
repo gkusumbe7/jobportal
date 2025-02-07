66d29aa7160355452694843e70c4ef07d596dca3
Ganesh Kusumbe <ganeshkusumbe@Ganeshs-MacBook-Air.local>
2025-02-06 22:49:43 -0800

    project

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonicModule] ,
+})
+export class HomePage {
+  constructor() {}
+}
