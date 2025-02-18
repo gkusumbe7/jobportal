e468f08444401ec1d9402ffe42a7516347648b57
Ganesh Kusumbe <ganeshkusumbe@Ganeshs-MacBook-Air.local>
2025-02-18 09:41:30 -0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { dashboardComponent } from "./pages/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [ dashboardComponent],
 })
 export class AppComponent {
   constructor() {}
