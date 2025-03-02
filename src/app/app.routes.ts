2d79e763cd2e44670af623d2cbcfff78efea73b1
Ganesh Kusumbe <ganeshkusumbe@Ganeshs-MacBook-Air.local>
2025-03-02 16:12:00 +0530

    added code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
-import {provideRouter , Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ApplyformsComponent } from './pages/applyforms/applyforms.component';
-import { MessagesComponent } from './pages/messages/messages.component';
+// import { MessagesComponent } from './pages/messages/messages.component';
 import { JobdetailsComponent } from './pages/jobdetails/jobdetails.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from 'src/app/shared/footer/footer.component';
 import { IonicModule } from '@ionic/angular';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   standalone:true,
-  imports:[FooterComponent , IonicModule]
+  imports:[FooterComponent , IonicModule , MatIconModule , CommonModule],
 })
 export class ProfileComponent  implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+  }
+
+  skills = [
+    { name: 'PHP', percentage: 86, color: '#E67E22' },
+    { name: 'Java', percentage: 48, color: '#27AE60' },
+    { name: 'MySQL', percentage: 56, color: '#2980B9' },
+    { name: 'React N', percentage: 34, color: '#9B59B6' },
+    { name: 'CSS', percentage: 86, color: '#5D3FD3' }
+  ];
+
+  // Circle properties
+  radius = 40;
+  circumference = 2 * Math.PI * this.radius;
+
+  getStrokeOffset(percentage: number): number {
+    return this.circumference - (percentage / 100) * this.circumference;
+  }
 
   ngOnInit() {}
 
