bd611a0a0f5e3beda57a529fcf7e04c4f49f1163
Ganesh Kusumbe <ganeshkusumbe@Ganeshs-MacBook-Air.local>
2025-04-06 00:02:34 +0530

    added chages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { ApplyformsComponent } from "./pages/applyforms/applyforms.component";
 import { FooterComponent } from "./shared/footer/footer.component";
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from "./pages/register/register.component";
 import { MessagesComponent } from "./pages/messages/messages.component";
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [
-    DashboardComponent,
-    // MessagesComponent
+    // DashboardComponent,
+    LoginComponent
 ],
 })
 export class AppComponent {
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [IonicModule, FormsModule, FooterComponent]
+  imports: [IonicModule, FormsModule, ]
 })
 export class LoginComponent  implements OnInit {
   username = '';
